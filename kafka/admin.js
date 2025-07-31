e3c0290a5273b6bafeee6bc628935374a722dfc5
dakshsawhneyy <darkstardaksh@gmail.com>
2025-07-31 12:40:16 +0000

    Added Admin.js and created topic[log-processing]

---
+++ kafka/admin.js
@@ -0,0 +1,24 @@
+const kafka = require('./client')
+
+async function init() {
+    const admin = kafka.admin();
+
+    console.log("Connecting Admin.......")
+    await admin.connect();
+    console.log("Connected to Admin 🎟️");
+
+    // Creation of topic
+    console.log("Creating topic [log-processing]....")
+    await admin.createTopics({
+        topics: [{
+            topic: 'log-processing',
+            numPartitions: 3    
+        }]
+    })
+    console.log("Topic[log-processing] created successfully 🎫")
+
+    // Disconnect Admin
+    console.log("Disconnecting Admin.......")
+    await admin.disconnect()
+    console.log("Disconnected Admin 🏁")
+}
