8b4e9e0b9947a630999000b671282b74b187c800
dakshsawhneyy <darkstardaksh@gmail.com>
2025-08-02 11:46:00 +0000

    Finitialized log_consumer

--- log_consumer.js
+++ log_consumer.js
@@ -1,5 +1,16 @@
 const { kafka } = require('./kafka/client')
 
+const { Pool } = require('pg')
+
+// Setup PostgreSQL connection
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mydb',
+    password: 'admin',
+    port: 5432,
+});
+
 async function run_consumer(){
     const consumer = kafka.consumer({groupId: 'log-group'});
 
@@ -15,6 +26,24 @@ async function run_consumer(){
             console.log('Recieved LOG', log)
         }
     })
+
+    // Insert into DB, if level is ERROR or CRITICAL
+    try {
+        if (log_message['level'] == 'ERROR' || log_message['level'] == 'CRITICAL'){
+            console.log('Inserting log into database ...');
+            await pool.query(
+                'INSERT INTO logs (created_at, level, message) VALUES ($1, $2, $3)',
+                [log_message['timestamp'], log_message['level'], log_message['message']]
+            )
+            console.log('Log inserted into database successfully😈😈')
+        }else{
+            console.log('Log level is not ERROR or CRITICAL, skipping database insertion.')
+        }
+    } catch (error) {
+        console.error('Error inserting log into database:', error.message)
+        throw new Error('Database insertion failed')
+    }
+   
 }
 
 run_consumer()
