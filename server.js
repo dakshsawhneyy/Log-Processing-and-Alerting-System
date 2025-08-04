d6472a350d13f47ed3f4cf00d1c7bc95bed25cb7
dakshsawhneyy <darkstardaksh@gmail.com>
2025-08-04 11:46:28 +0000

    Added /metrics endpoint in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sendLogToKafka = require('./log_producer');
+const { register } = require('prom-client');
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(express.json())     // without this body will be undefined {}
 
 // node js handles /log request
 // this recieves the data from api parses it and send it to kafka, acts as middleware
-app.use('/log', async(req, res) => {    // this middleware parses incoming JSON into req.body
+app.use('/log', async(req,res) => {    // this middleware parses incoming JSON into req.body
     const logData = req.body;
     try {
         await sendLogToKafka(logData);
@@ -16,6 +17,18 @@ app.use('/log', async(req, res) => {    // this middleware parses incoming JSON 
     } catch (error) {
         console.error('Error sending log data to Kafka:', error);
         res.status(500).json({message: 'Failed to send log data to Kafka'});
+    }
+})
+
+// Adding prom-client 
+app.use('/metrics', async(req,res) => {
+    try {
+        // this is the format that prometheus understands
+        res.set('Content-Type', register.contentType);   // thats how prometheus expects data to recieve data
+        res.end(await register.contentType);   // sends that data as a final response to Prometheus
+    } catch (error) {
+        console.error('Error fetching metrics:', error);
+        res.status(500).json({message: 'Failed to fetch metrics'});
     }
 })
 
